d14f0bc47a8a46dc742016ed78a1848e29c07f96
Sergio Barrera <sdbeng@gmail.com>
2019-06-20 17:38:06 -0700

    create /all route

--- server.js
+++ server.js
@@ -15,15 +15,30 @@ var collections = ['scrapedData']
 // Hook mongojs configuration to the db variable
 var db = mongojs(databaseUrl, collections)
 db.on('error', function (error) {
-  console.log('Database Error:', error)
+    console.log('Database Error:', error)
 })
 
 // Main route (simple Hello World Message)
 app.get('/', function (req, res) {
-  res.send('Hello world')
+    res.send('Hello world')
+})
+
+// Retrieve data from the db
+app.get('/all', function (req, res) {
+    // Find all results from the scrapedData collection in the db
+    db.scrapedData.find({}, function (error, found) {
+        // Throw any errors to the console
+        if (error) {
+            console.log(error)
+        }
+        // If there are no errors, send the data to the browser as json
+        else {
+            res.json(found)
+        }
+    })
 })
 
 // Listen on port 3000
 app.listen(3030, function () {
     console.log('App running on port 3000!')
-  })
+})
